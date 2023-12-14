2401412a03083f0c2cc65e4ff51016426971c77a
hadzikk <hadzikmochammad@gmail.com>
2023-12-14 10:53:27 +0700

    Menambahan foreach untuk menampilkan data kategori 1

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+    include 'conn.php';
+    
+    $data = mysqli_query($conn, "SELECT * FROM buku");
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -23,21 +29,11 @@
         <div class="cards-container mt-16">
             <p>Kategori 1</p>
             <div class="cards min-w-full min-h-32 mb-16 flex overflow-y-hidden">
+                <?php foreach($data as $d) : ?>
                 <div class="card w-52 h-full rounded mx-1">
-                    <img class="h-full rounded" src="https://img.freepik.com/free-photo/open-book-concept-fiction-storytelling-fairytale_23-2150793749.jpg?t=st=1701241111~exp=1701244711~hmac=caec36dd79245cd7c526363ba0b4109480d83b9763ec85f18c56b2d84de792e3&w=826" alt="">
+                    <img class="h-full rounded" src="<?php echo $d["gambar_buku"];?>" alt="">
                 </div>
-                <div class="card w-52 h-full mx-1">
-                    <img class="h-full rounded" src="https://img.freepik.com/free-photo/open-book-concept-fiction-storytelling-fairytale_23-2150793749.jpg?t=st=1701241111~exp=1701244711~hmac=caec36dd79245cd7c526363ba0b4109480d83b9763ec85f18c56b2d84de792e3&w=826" alt="">
-                </div>
-                <div class="card w-52 h-full mx-1">
-                    <img class="h-full rounded" src="https://img.freepik.com/free-photo/open-book-concept-fiction-storytelling-fairytale_23-2150793749.jpg?t=st=1701241111~exp=1701244711~hmac=caec36dd79245cd7c526363ba0b4109480d83b9763ec85f18c56b2d84de792e3&w=826" alt="">
-                </div>
-                <div class="card w-52 h-full mx-1">
-                    <img class="h-full rounded" src="https://img.freepik.com/free-photo/open-book-concept-fiction-storytelling-fairytale_23-2150793749.jpg?t=st=1701241111~exp=1701244711~hmac=caec36dd79245cd7c526363ba0b4109480d83b9763ec85f18c56b2d84de792e3&w=826" alt="">
-                </div>
-                <div class="card w-52 h-full mx-1">
-                    <img class="h-full rounded" src="https://img.freepik.com/free-photo/open-book-concept-fiction-storytelling-fairytale_23-2150793749.jpg?t=st=1701241111~exp=1701244711~hmac=caec36dd79245cd7c526363ba0b4109480d83b9763ec85f18c56b2d84de792e3&w=826" alt="">
-                </div>
+                <?php endforeach;?>
             </div>
         </div>
     </section>
